439b37b72ce19cf69ee55c9aaa649208bcc21a51
Гордієнко Олексій <114742171+HordienkoO@users.noreply.github.com>
2024-05-17 22:04:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/register', async (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/login', async (req, res) => {
